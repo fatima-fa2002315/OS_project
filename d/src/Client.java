222f1a4cfde17e78f76d9e193894608fdf6fa069
Saja Abdelmailk <112798099+sa1808179@users.noreply.github.com>
2024-05-05 14:07:09 +0300

    Update Client.java

--- d/src/Client.java
+++ d/src/Client.java
@@ -1,3 +1,5 @@
+package OS;
+
 import java.util.*;
 import java.io.*;
 import java.net.*;
@@ -9,159 +11,164 @@ public class Client {
     private Scanner kb;
 
     public Client(String ip, int port) throws Exception {
-        this.sock = new Socket(ip,port);
+        this.sock = new Socket(ip, port);
         this.in = new Scanner(sock.getInputStream(), "UTF-8");
         this.out = new PrintStream(sock.getOutputStream(), true, "UTF-8");
         this.kb = new Scanner(System.in);
     }
 
-    public static void main(String [] args) {
-        if(args.length < 2) {
+    public static void main(String[] args) {
+        if (args.length < 2) {
             System.out.println("Usage: java Client <IP address> <port>");
             return;
         }
 
         try {
             Client client = new Client(args[0], Integer.parseInt(args[1]));
-            System.out.println("CONNECTED TO SERVER\n");
-
+            System.out.println("CLIENT CONNECTED TO SERVER \n");
             client.start();
-
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     private void start() throws Exception {
-        while(true) {
+        while (true) {
             System.out.println("\n1. Have existing ticket");
             System.out.println("2. Get new ticket");
-            System.out.print("? ");
-            String s = kb.next();
-            if(s.equals("1")) {
+            System.out.println("3. Exit");
+            String choice = kb.next();
+            if (choice.equals("1")) {
                 System.out.print("Enter ticket number: ");
-                int tick = kb.nextInt();
-                out.println("ID " + tick);
-                String ans = in.nextLine();
-                if(ans.startsWith("FAIL")) {
+                int ticket = kb.nextInt();
+                out.println("ID " + ticket);
+                String response = in.nextLine();
+                if (response.startsWith("FAIL")) {
                     System.out.println("Invalid ticket number");
                 } else {
                     System.out.println("Login successful!");
-                    System.out.println(ans + "\n");
+                    System.out.println(response + "\n");
                     break;
                 }
-            } else if(s.equals("2")) {
+            } else if (choice.equals("2")) {
                 System.out.print("Enter nickname: ");
                 String name = kb.next();
                 out.println("NAME " + name);
-                String ans = in.nextLine();
+                String response = in.nextLine();
                 System.out.println("Login successful!");
-                System.out.println(ans + "\n");
+                System.out.println(response + "\n");
                 break;
+            } else if (choice.equals("3")) {
+                System.out.println("Exiting...");
+                out.close();
+                in.close();
+                sock.close();
+                System.exit(0);
             } else {
                 System.out.println("Invalid choice");
             }
         }
 
-        // logged in now, fetch all the info from server and print it
-        int blankLines = 0;
-        while(blankLines < 3) {
-            String s = in.nextLine();
-            System.out.println(s);
-            if(s.length() == 0)
-                blankLines ++;
-        }
+        mainLoop();
+    }
 
-        while(true) {
+    private void mainLoop() throws Exception {
+        while (true) {
             System.out.println("\n\n1. Get list of games");
             System.out.println("2. Get list of players");
             System.out.println("3. Join a game");
             System.out.println("4. Create a new game");
             System.out.println("5. Logout");
-            System.out.print("? ");
+            System.out.println("6. Exit");
 
-            String s = kb.next();
+            String choice = kb.next();
 
-            if(s.equals("1") || s.equals("2")) {
-                if(s.equals("1")) {
-                    out.println("GAMES");
-                } else {
-                    out.println("PLAYERS");
-                }
-
-                while(true) {
-                    String l = in.nextLine();
-                    System.out.println(l);
-                    if(l.length() == 0)
-                        break;
-                }
-            } else if(s.equals("3") || s.equals ("4")) {
-                if(s.equals("3")) {
-                    System.out.print("Enter game number to join: ");
-                    int game = kb.nextInt();
-                    out.println("JOIN " + game);
-                } else {
-                    System.out.print("Enter the game name: ");
-                    String n = kb.next();
-                    out.println("CREATE " + n);
-                }
-                String ans = in.nextLine();
-                if(ans.startsWith("FAIL")) {
-                    if(s.equals("3")) {
-                        System.out.println("FAILED TO JOIN GAME");
-                    } else {
-                        System.out.println("FAILED TO CREATE GAME");
-                    }
-                } else {
-                    if(s.equals("3")) {
-                        System.out.println("Joined the game\n");
-                    } else {
-                        System.out.println("Created the game\n");
-                    }
-                    System.out.println(ans);
-                    while(true) {
-                        String l = in.nextLine();
-                        System.out.println(l);
-                        if(l.length() == 0)
-                            break;
-                    }
-                    System.out.println("Waiting for game to start...\n");
-                    playGame();
-                }
-            } else if(s.equals("4")) {
-                System.out.print("Enter the game name: ");
-                String n = kb.next();
-                out.println("CREATE " + n);
-                String ans = in.nextLine();
-            } else if(s.equals("5")) {
-                out.println("LEAVE");
-                sock.close();
-                System.exit(0);
-            } else {
-                System.out.println("Invalid option");
+            switch (choice) {
+                case "1":
+                case "2":
+                    out.println(choice.equals("1") ? "GAMES" : "PLAYERS");
+                    printServerResponse();
+                    break;
+                case "3":
+                    joinGame();
+                    break;
+                case "4":
+                    createGame();
+                    break;
+                case "5":
+                    logout();
+                    break;
+                case "6":
+                    System.out.println("Exiting...");
+                    out.close();
+                    in.close();
+                    sock.close();
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid option");
             }
         }
     }
 
+    private void joinGame() throws Exception {
+        System.out.print("Enter game number to join: ");
+        int game = kb.nextInt();
+        out.println("JOIN " + game);
+        handleGameResponse();
+    }
+
+    private void createGame() throws Exception {
+        System.out.print("Enter the game name: ");
+        String name = kb.next();
+        out.println("CREATE " + name);
+        handleGameResponse();
+    }
+
+    private void handleGameResponse() throws Exception {
+        String response = in.nextLine();
+        if (response.startsWith("FAIL")) {
+            System.out.println(response);
+        } else {
+            System.out.println(response);
+            printServerResponse();
+            System.out.println("Waiting for game to start...\n");
+            playGame();
+        }
+    }
+
+    private void printServerResponse() throws Exception {
+        String line;
+        while (true) {
+            line = in.nextLine();
+            System.out.println(line);
+            if (line.length() == 0)
+                break;
+        }
+    }
+
+    private void logout() throws Exception {
+        out.println("LEAVE");
+        sock.close();
+        System.exit(0);
+    }
+
     private void playGame() throws Exception {
-        boolean started = false;
-        while(true) {
-            String a = in.nextLine();
-            if(a.startsWith("GAME")) { // GAME-OVER
-                try {
-                    Thread.sleep(30*1000);
-                } catch(InterruptedException e) {
-                }
+        while (true) {
+            String action = in.nextLine();
+            if (action.startsWith("GAME")) { // GAME-OVER
                 System.out.println("GAME OVER");
                 return;
             }
-            if(a.startsWith("ROUND")) {
-                System.out.print(a + " - Enter your number: ");
+            if (action.startsWith("ROUND")) {
+                System.out.print(action + " - Enter your number: ");
                 int num = kb.nextInt();
                 out.println("PLAY " + num);
             } else {
-                System.out.println(a);
+                System.out.println(action);
             }
         }
     }
 }
+
+
